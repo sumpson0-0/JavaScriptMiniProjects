6a5a62ab424371d86c023fde7c26a1efa8acf3b3
sumpson0-0 <sumpson00@gmail.com>
2020-07-23 22:11:17 +0900

    동적인 element 추가하여 데이터 값 지정

--- infinite-scroll/assets/index.js
+++ infinite-scroll/assets/index.js
@@ -1,29 +1,51 @@
-const giveData = (name, money, price) => {
-	console.log(name, money, price);
+const container = document.querySelector('.coin-container');
+
+// 로딩 구현하기
+const createContent = (name, kind, value) => {
+	const content = document.createElement('article');
+	content.className = 'coin';
+	const coin = document.createElement('h4');
+	coin.className = 'name';
+	coin.innerText = name;
+	const div = document.createElement('div');
+	div.className = 'price-container';
+	const money = document.createElement('p');
+	money.className = 'money';
+	money.innerText = kind;
+	const price = document.createElement('p');
+	price.className = 'price';
+	price.innerText = `$${value}`;
+	content.appendChild(coin);
+	div.appendChild(money);
+	div.appendChild(price);
+	content.appendChild(div);
+	container.appendChild(content);
 };
 
-const init = () => {
+const giveData = json => {
+	json.forEach(async coin => {
+		const name = coin.name;
+		const {
+			quotes,
+			quotes: {
+				USD: { price },
+			},
+		} = coin;
+		const keys = Object.keys(quotes);
+		const money = keys[0];
+		await createContent(name, money, price);
+	});
+};
+
+const getData = () => {
 	fetch(`https://api.coinpaprika.com/v1/tickers`)
 		.then(response => {
 			if (response && response.ok) {
 				return response.json();
 			}
 		})
-		.then(json => {
-			json.forEach(coin => {
-				const name = coin.name;
-				const {
-					quotes,
-					quotes: {
-						USD: { price },
-					},
-				} = coin;
-				const keys = Object.keys(quotes);
-				const money = keys[0];
-				giveData(name, money, price);
-			});
-		})
+		.then(json => giveData(json))
 		.catch(error => console.log(error));
 };
 
-init();
+getData();
